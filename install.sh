bcd6d2165e1ddde10d91f3ccd87481d5440e6ba3
kevdagoat <42062334+kevdagoat@users.noreply.github.com>
2018-08-05 16:05:40 +0800

    add install script

--- install.sh
+++ install.sh
@@ -1,4 +1,61 @@
 #!/bin/sh
+touch /bin/ledctl
+touch /bin/systemctl
+
+cat > /bin/ledctl <<EOF
+#!/bin/sh
+
+leds=$(ls /sys/class/leds) # Find avaliable leds
+
+################################################
+
+if [ "$1" = -l ]; then                     #Check for list command.
+echo $leds   
+exit 1   
+fi
+
+################################################
+
+if [ -z $1 ]; then                                                             #Check if user inputted any led data.
+echo "Please select LED. To see all avaliable LEDs run: ledctl -l"
+exit 1
+fi
+
+################################################
+
+if [ -z $2 ]; then                        #Check if state exists
+   echo "Please choose state: on or off."
+exit 1
+fi
+    
+################################################
+
+if [ -e /sys/class/leds/$1 ]; then       #Check if spcified led exists.
+ echo 'Led exists.'
+
+      if [ "$2" == 'on' ] && [ -e /sys/class/leds/$1 ]; then    #Check if args are valid and write data
+        ledstate=1
+        echo $ledstate >/sys/class/leds/$1/brightness
+        echo "Set "$1" state to "$2"."
+
+      elif [ "$2" == 'off' ] && [ -e /sys/class/leds/$1 ]; then  #Check if args are valid and write data
+        ledstate=0
+        echo $ledstate >/sys/class/leds/$1/brightness
+        echo "Set "$1" state to "$2"."
+      else 
+        echo "Please choose state: on or off."
+        exit 1
+      fi
+
+else
+ echo 'Led selection is invalid. Run ledcontrol -l to see avaliable leds.' 
+ exit 1
+fi
+################################################
+EOF
+
+cat > /bin/systemctl <<EOF
+#!/bin/sh
 
 processes=$(ls /etc/init.d/) # Find avaliable services
 
@@ -80,3 +137,6 @@ else
 fi
 ################################################
 
+
+EOF
+echo "Install finished. To use commands run systemctl/ledctl -h"
